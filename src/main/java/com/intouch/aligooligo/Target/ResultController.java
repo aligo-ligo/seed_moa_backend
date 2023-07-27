c02ae46dac88aff00e02cc00d5e612b8ae888636
icecoff22 <wjdtmd987@gmail.com>
2023-07-27 17:45:29 +0900

    refactor : ResultController

--- src/main/java/com/intouch/aligooligo/Target/ResultController.java
+++ src/main/java/com/intouch/aligooligo/Target/ResultController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 public class ResultController {
 
     private final TargetService targetService;
+    
     @GetMapping
     public ResponseEntity<TargetDTO> resultTargetPage(@RequestParam Integer id){
         try{
