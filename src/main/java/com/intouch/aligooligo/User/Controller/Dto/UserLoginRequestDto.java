1bee73ca93ae4fbdcead77c00f870e778230b694
icecoff22 <wjdtmd987@gmail.com>
2024-01-01 02:12:50 +0900

    [rename] UserLoginDto

--- src/main/java/com/intouch/aligooligo/User/Controller/Dto/UserLoginRequestDto.java
+++ src/main/java/com/intouch/aligooligo/User/Controller/Dto/UserLoginRequestDto.java
@@ -1,2 +1,12 @@
-package com.intouch.aligooligo.User.Controller.Dto;public class UserLoginRequestDto {
+package com.intouch.aligooligo.User.Controller.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLoginRequestDto {
+    private String email;
+    private String password;
+    private String nickName;
 }
--- src/test/java/com/intouch/aligooligo/UserTest/UserControllerTest.java
+++
@@ -1,2 +0,0 @@
-package com.intouch.aligooligo.UserTest;public class UserControllerTest {
-}
