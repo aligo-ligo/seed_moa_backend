2eae4b08910901155d1974f155d0571739b20f09
Icecoff22 <icecoff2222@gmail.com>
2024-05-05 03:44:53 +0900

    [FEAT] 루틴 상세페이지 응답 구조 구현

--- src/main/java/com/intouch/aligooligo/seed/controller/dto/response/SeedDetailResponse.java
+++ src/main/java/com/intouch/aligooligo/seed/controller/dto/response/SeedDetailResponse.java
@@ -1,5 +1,16 @@
 package com.intouch.aligooligo.seed.controller.dto.response;
 
-public class SeedDetailResponse {
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
+public class SeedDetailResponse {
+    private String seed;
+    private String startDate;
+    private String endDate;
+    private Integer completedRoutineCount;
+    private Map<String, Boolean> routines;
+    private String state;
 }
