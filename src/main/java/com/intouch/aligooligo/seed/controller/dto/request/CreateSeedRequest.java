dd3a44fc6729282bdf39ba0e32fa818c101927a2
Icecoff22 <icecoff2222@gmail.com>
2024-05-05 01:39:39 +0900

    [FEAT] seed 생성 요청 dto 추가

--- src/main/java/com/intouch/aligooligo/seed/controller/dto/request/CreateSeedRequest.java
+++ src/main/java/com/intouch/aligooligo/seed/controller/dto/request/CreateSeedRequest.java
@@ -1,2 +1,13 @@
-package com.intouch.aligooligo.seed.controller.dto.request;public class CreateSeedRequest {
+package com.intouch.aligooligo.seed.controller.dto.request;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreateSeedRequest {
+    private String endDate;
+    private String seed;
+    private List<String> routines;
 }
