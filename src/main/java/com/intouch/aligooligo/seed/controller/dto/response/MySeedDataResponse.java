5bb0520416eb0eeb22cbfbad5cde9a38a05fd4dd
Icecoff22 <icecoff2222@gmail.com>
2024-05-06 01:59:47 +0900

    [FEAT] 내 데이터 조회 시 응답 구조 추가

--- src/main/java/com/intouch/aligooligo/seed/controller/dto/response/MySeedDataResponse.java
+++ src/main/java/com/intouch/aligooligo/seed/controller/dto/response/MySeedDataResponse.java
@@ -1,2 +1,20 @@
-package com.intouch.aligooligo.seed.controller.dto.response;public class MySeedDataResponse {
+package com.intouch.aligooligo.seed.controller.dto.response;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MySeedDataResponse {
+    private String email;
+    private String name;
+    private List<StateStatistics> stateStatisticsList;
+
+    @AllArgsConstructor
+    @Getter
+    public static class StateStatistics {
+        private String state;
+        private Long stateCount;
+    }
 }
