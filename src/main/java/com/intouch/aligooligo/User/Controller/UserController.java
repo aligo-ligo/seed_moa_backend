e631b1ff492770bfb8e765f8e927a47876774d09
Icecoff22 <icecoff2222@gmail.com>
2024-05-06 03:35:28 +0900

    [FEAT] 임시 유저 로그인 API 추가

--- src/main/java/com/intouch/aligooligo/User/Controller/UserController.java
+++ src/main/java/com/intouch/aligooligo/User/Controller/UserController.java
@@ -1,10 +1,31 @@
 package com.intouch.aligooligo.User.Controller;
 
+import com.intouch.aligooligo.Jwt.JwtTokenProvider;
+import com.intouch.aligooligo.User.Entity.User;
+import com.intouch.aligooligo.User.Repository.UserRepository;
+import com.intouch.aligooligo.auth.dto.TokenInfo;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RequestMapping(value="/users")
 @RestController
 public class UserController {
+    private final UserRepository userRepository;
+    private final JwtTokenProvider jwtProvider;
+    @PostMapping
+    public ResponseEntity<?> login(@RequestParam("email") String admin) {
+        User user = userRepository.findByEmail(admin).get();
+        List<String> roles = new ArrayList<>();
+        TokenInfo tokenInfo = null;
+        if(user.getEmail().equals(admin)) {
+            tokenInfo = jwtProvider.createToken(admin, roles);
+        }
+        return new ResponseEntity<>(tokenInfo, HttpStatus.OK);
+    }
 }
