c565b6e1257b74d03572bf4d744149104106c85b
Icecoff22 <icecoff2222@gmail.com>
2024-05-06 04:01:08 +0900

    [REFACTOR] 루틴 수정 API 수정
    - seed에서 seed/routine으로 세부적으로 변경
    - 업데이트 시 시간 로그 찍힘
    - tokenInfo 분리
    - db에서 data 찾지 못할 때 custom 예외 클래스 생성

--- src/main/java/com/intouch/aligooligo/exception/DataNotFoundException.java
+++ src/main/java/com/intouch/aligooligo/exception/DataNotFoundException.java
@@ -1,2 +1,8 @@
-package com.intouch.aligooligo.exception;public class DataNotFoundException {
+package com.intouch.aligooligo.exception;
+
+public class DataNotFoundException extends RuntimeException{
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+
 }
--- src/main/java/com/intouch/aligooligo/seed/service/RoutineService.java
+++ src/main/java/com/intouch/aligooligo/seed/service/RoutineService.java
@@ -1,2 +1,31 @@
-package com.intouch.aligooligo.seed.service;public class RoutineService {
+package com.intouch.aligooligo.seed.service;
+
+import com.intouch.aligooligo.exception.DataNotFoundException;
+import com.intouch.aligooligo.seed.controller.dto.request.UpdateSeedRequest;
+import com.intouch.aligooligo.seed.domain.Routine;
+import com.intouch.aligooligo.seed.domain.RoutineTimestamp;
+import com.intouch.aligooligo.seed.repository.RoutineRepository;
+import com.intouch.aligooligo.seed.repository.RoutineTimestampRepository;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+public class RoutineService {
+
+    private final RoutineRepository routineRepository;
+    private final RoutineTimestampRepository routineTimestampRepository;
+
+    @Transactional
+    public void updateSeed(Long routineId, UpdateSeedRequest updateSeedRequest) {
+        Routine routines = routineRepository.findById(routineId)
+                .orElseThrow(() -> new DataNotFoundException("can't find routine"));
+
+        routines.updateRoutine(updateSeedRequest.getRoutineTitle());
+        routineTimestampRepository.save(new RoutineTimestamp(LocalDate.now(), routines));
+    }
+
 }
