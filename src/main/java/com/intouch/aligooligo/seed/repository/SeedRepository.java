287222274141abceb10ed9aaa40fc2b6bb18c794
Icecoff22 <icecoff2222@gmail.com>
2024-05-06 02:00:06 +0900

    [FEAT] 내 데이터 조회 API 추가

--- src/main/java/com/intouch/aligooligo/seed/repository/SeedRepository.java
+++ src/main/java/com/intouch/aligooligo/seed/repository/SeedRepository.java
@@ -1,11 +1,13 @@
 package com.intouch.aligooligo.seed.repository;
 
 import com.intouch.aligooligo.seed.domain.Seed;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SeedRepository extends JpaRepository<Seed, Long> {
-    Page<Seed> findByUserIdOrderByIdDesc(Integer userId, Pageable pageable);
+    Page<Seed> findByUserIdOrderByIdDesc(Long userId, Pageable pageable);
+    List<Seed> findByUserId(Long userId);
     boolean existsById(Integer targetId);
 }
