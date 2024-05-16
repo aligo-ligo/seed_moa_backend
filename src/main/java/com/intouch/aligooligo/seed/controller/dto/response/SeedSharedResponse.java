c3e84cc80a2edcccf90081b367961e00ce20e987
Icecoff22 <icecoff2222@gmail.com>
2024-05-16 10:53:25 +0900

    [FEAT] 유저 공유 api 추가

--- src/main/java/com/intouch/aligooligo/seed/controller/dto/response/SeedSharedResponse.java
+++ src/main/java/com/intouch/aligooligo/seed/controller/dto/response/SeedSharedResponse.java
@@ -1,2 +1,26 @@
-package com.intouch.aligooligo.seed.controller.dto.response;public class SeedSharedResponse {
+package com.intouch.aligooligo.seed.controller.dto.response;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class SeedSharedResponse {
+    private String seed;
+    private String startDate;
+    private String endDate;
+    private Integer completedRoutineCount;
+    private String seedState;
+    private List<SharedRoutineDetail> routineDetails;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class SharedRoutineDetail {
+        private Long routineId;
+        private String routineTitle;
+    }
 }
