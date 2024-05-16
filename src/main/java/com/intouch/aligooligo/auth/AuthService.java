f64fb9791d3bc1fddf4011efdce8846e04865918
Icecoff22 <icecoff2222@gmail.com>
2024-05-17 01:09:52 +0900

    [REFACTOR] 모바일 환경 테스트 위한 정적 kakao redirect uri 설정

--- src/main/java/com/intouch/aligooligo/auth/AuthService.java
+++ src/main/java/com/intouch/aligooligo/auth/AuthService.java
@@ -104,7 +104,8 @@ public class AuthService {
         body.add("grant_type","authorization_code");
         body.add("client_id", clientId);
         body.add("client_secret", clientSecret);
-        body.add("redirect_uri", redirectUrl);
+        body.add("redirect_uri", "http://192.168.45.196:5173/kakao");
+        //body.add("redirect_uri", redirectUrl);
         body.add("code",code);
 
         HttpEntity<MultiValueMap<String, String>> tokenRequest = new HttpEntity<>(body, headers);
