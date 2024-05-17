a39c915c4662bba290e39c07263edf2ef153affb
Icecoff22 <icecoff2222@gmail.com>
2024-05-17 16:30:04 +0900

    [FIX] 토큰 재발급 무한로딩되는 버그 픽스

--- src/main/java/com/intouch/aligooligo/Jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/intouch/aligooligo/Jwt/JwtAuthenticationFilter.java
@@ -25,7 +25,7 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
         try {
             String token = jwtTokenProvider.resolveAccessToken((HttpServletRequest) request);
             // 유효한 토큰인지 확인합니다.
-            if (token != null && jwtTokenProvider.validateToken(token)) {
+            if (token != null && !token.equals("null") && jwtTokenProvider.validateToken(token)) {
                 // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
                 Authentication authentication = jwtTokenProvider.getAuthentication(token);
                 // SecurityContext 에 Authentication 객체를 저장합니다.
@@ -42,7 +42,7 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json;charset=UTF-8");
         PrintWriter writer = response.getWriter();
-        writer.write("{\"error\":\"" + msg + "\"}");
+        writer.write("{\"message\":\"" + msg + "\"}");
         writer.flush();
         writer.close();
     }
