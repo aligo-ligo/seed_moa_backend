9eca0c8ed63ec8e244629ab84860c83b49edddfc
Icecoff22 <icecoff2222@gmail.com>
2024-05-20 17:50:02 +0900

    [REFACTOR] 응원 유저 정보대신 유저 카운트로 변경

---
+++ src/main/java/com/intouch/aligooligo/seed/controller/dto/response/CheerInfo.java
@@ -0,0 +1,19 @@
+package com.intouch.aligooligo.seed.controller.dto.response;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CheeringInfo {
+
+    private List<CheeringList>
+    private Integer cheerCount;
+
+    @Getter
+    @AllArgsConstructor
+    public static class CheeringList {
+
+    }
+}
